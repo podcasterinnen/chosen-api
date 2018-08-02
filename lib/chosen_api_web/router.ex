90e20a283f0c82a41c32ef449ee644916b3bba3c
Michaela Lehr <michi_lehr@web.de>
2018-08-02 11:11:53 +0200

    FIX cors plug, MOD login message

--- lib/chosen_api_web/router.ex
+++ lib/chosen_api_web/router.ex
@@ -2,7 +2,6 @@ defmodule ChosenApiWeb.Router do
   use ChosenApiWeb, :router
 
   pipeline :api do
-    plug CORSPlug, [origin: "http://localhost:3000"]
     plug :accepts, ["json"]
     plug :fetch_session
     plug :fetch_flash
