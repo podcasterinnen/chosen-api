6e4bef6f35e7657aad4a0cc630b51f9d90310344
Michaela Lehr <michi_lehr@web.de>
2018-12-15 13:06:59 +0100

    #38 FIX PUT podcasterin secured via id check

--- lib/chosen_api_web/controllers/podcaster_controller.ex
+++ lib/chosen_api_web/controllers/podcaster_controller.ex
@@ -8,8 +8,7 @@ defmodule ChosenApiWeb.PodcasterController do
 
   action_fallback ChosenApiWeb.FallbackController
 
-  # TODO use :id_check
-  plug :user_check when action in [:update]
+  plug :id_check when action in [:update]
 
   def index(conn, %{"user_id" => user_id}) do
     podcaster = Profiles.get_podcaster_by_user_id!(user_id)
