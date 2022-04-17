ed97da90cba295690f065d458a7c8ff23fee30b3
hukl <contact@smyck.org>
2022-04-17 12:08:04 +0200

    fix dynamic IP address format

--- config/runtime.exs
+++ config/runtime.exs
@@ -58,7 +58,7 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: System.get_env("HOST"),
+      ip: System.get_env("HOST") |> String.split(".") |> Enum.map(&String.to_integer/1) |> List.to_tuple(),
       port: port
     ],
     secret_key_base: secret_key_base
